fdaddc2a63e6fc6a24d3113ca947e8ce4045da71
Dwi Didit Prasetiyo <didit@dwidi.com>
2024-05-25 13:19:55 +0700

    Edit CORS

--- src/main/java/com/dwidi/nasaasteroidsapi/config/CorsConfig.java
+++ src/main/java/com/dwidi/nasaasteroidsapi/config/CorsConfig.java
@@ -2,6 +2,7 @@ package com.dwidi.nasaasteroidsapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -10,7 +11,8 @@ import org.springframework.web.filter.CorsFilter;
 public class CorsConfig {
 
     @Bean
-    public CorsFilter corsFilter() {
+    @Profile("dev")
+    public CorsFilter devCorsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
@@ -20,5 +22,19 @@ public class CorsConfig {
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
+
+    @Bean
+    @Profile("prod")
+    public CorsFilter prodCorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("https://asteroid.my.id");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 }
+
 
